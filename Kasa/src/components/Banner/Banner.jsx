aa9154423e586f62db71dbe14e86d4b31ca968c7
kyllianschneider <98122428+kyllianschneider@users.noreply.github.com>
2024-02-11 15:40:37 +0100

    fixing the banners and the header
    i had new bug tho, the about page now act weirdly it doesnt seems to fill the whole screen

--- Kasa/src/components/Banner/Banner.jsx
+++ Kasa/src/components/Banner/Banner.jsx
@@ -1,11 +1,14 @@
 import "../Banner/banner.scss";
+import {useLocation } from "react-router-dom";
+
+
 
 function Banner({ texte }) {
-    const Route = "/";
+    const {pathname} = useLocation();
 
     return (
        
-        <div className={`banner ${Route === "/" ? "banner--home" : "banner--about"}`}>
+        <div className={`banner ${pathname === "/" ? "banner--home" : "banner--about"}`}>
 
             <div className="banner__overlay"></div>
             <span className="banner__title">{texte}</span>
